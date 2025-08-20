4ce5e5ef158918e62243635e2c4933e77edbceb6
KvdMeijden-Web <k.vandermeijden@isalmere.nl>
2025-08-20 13:32:48 +0200

    Lesson 1 and 3 added
    Pushed lesson 1+3

---
+++ Lessons/Lesson_3_Patterns/main.py
@@ -0,0 +1,39 @@
+# print a message to all your classmates Hello, Peter!, Hello Sandra! etc, etc. Each on a new line with a string.
+
+
+
+
+# Do you recognize this pattern? What part is the same? What part is different?
+# [Answer]
+
+# Functions are useful for simplifying repeated tasks and patterns. 
+# Many functions are build-in, like print(), but you can also create your own functions.
+# A coder recognizes patterns in code and can use functions to simplify the code.
+# A function is created at the top of the file and can be called anywhere in the file.
+# the def keyword is used to define a function: "def function_name(parameters):
+
+def hello_function():
+    print("Hello")
+
+hello_function()  # Call the function
+    
+# write a function that prints: Welcome!, and call the function 3 times. Name the function welcome_function
+
+
+
+
+
+
+# You can insert parameters into a function, to add variable information.
+
+def hello_classmate(name):
+    print(f"Hello {name}!")
+
+hello_classmate("Peter")
+
+
+
+
+
+
+
