cee671b4afe8874bb438a0970f4f909cee2cd687
KvdMeijden-Web <k.vandermeijden@isalmere.nl>
2025-08-20 22:13:30 +0200

    New lesson 1
    First homework

---
+++ Lessons/Lesson_1_Intro/Main.py
@@ -0,0 +1,22 @@
+# ------------------------------------
+# Homework: lesson 1 - introduction to Python
+# ------------------------------------
+# Bandname generator.
+# The user is asked to input their city and pet name. Then the two combined will be the band name.
+
+# 1: Print a welcome message to the user.
+
+
+
+# 2: Ask the user for the name of the city they grew up in. safe this input in a variable called `city`.
+
+
+
+# 3: Ask the user for the name of a pet. safe this input in a variable called `pet`.
+
+
+
+# 4: Combine the two variables `city` and `pet` to create a band name. You can use string concatenation.
+
+favorite_color = input("what is your favorite color?")
+print (favorite_color)
